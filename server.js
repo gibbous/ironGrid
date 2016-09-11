219240d80be0f20d11dbce678063b3ca0a3abde2
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-09-10 22:24:41 -0400

    updating PORT reference

--- server.js
+++ server.js
@@ -166,6 +166,6 @@ app.post('/articles/:id', function(req, res){
 
 
 // listen on port 3000
-app.listen(3000, function() {
-  console.log('App running on port 3000!');
+//app.listen(3000, function() {
+  //console.log('App running on port 3000!');
 });
